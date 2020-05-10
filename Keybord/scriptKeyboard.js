a4f48e36f65ea3a9ff5e38c89e1a6f6fba7ebcc7
Your Name <you@example.com>
2020-05-10 21:02:48 +0300

    feat: add files

---
+++ Keybord/scriptKeyboard.js
@@ -0,0 +1,11 @@
+'use strict'
+let script = document.createElement('script');
+script.src = "Keybord/classes.js"
+document.head.append(script);
+
+window.onload = function() {
+    let script = document.createElement('script');
+    script.src = "Keybord/Keyboard.js"
+    document.head.append(script);
+
+}
