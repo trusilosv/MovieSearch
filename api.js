1932fc5647a53f3daf7738f3873a069296dd0dc4
Your Name <you@example.com>
2020-05-13 20:57:31 +0300

    fix:  logic fix

--- api.js
+++ api.js
@@ -1,3 +1,5 @@
+'use strict';
+var slider;
 async function getMovie(str, page) {
     const url = `https://www.omdbapi.com/?s=${str}&page=${page}&apikey=6bbdd328`;
     let res = await fetch(url);
@@ -23,45 +25,82 @@ class Movie {
         this.dom_year.innerHTML = year;
         this.dom_rating.innerHTML = rating;
         this.dom_img.src = "no-cover.png";
+        this.domadd = false;
     }
     dom_add(node = document.body) {
         this.dom_element.classList.add('movie');
+        this.dom_title.classList.add('title');
+        this.dom_year.classList.add('text');
+        this.dom_rating.classList.add('text');
         this.dom_element.appendChild(this.dom_title);
         this.dom_element.appendChild(this.dom_img);
         this.dom_element.appendChild(this.dom_year);
         this.dom_element.appendChild(this.dom_rating);
+
         if (this.imgurl != "N/A")
             this.dom_img.src = this.imgurl;
-        this.dom_img.onload(node.appendChild(this.dom_element));
-        this.dom_img.classList.add('movie_img');
 
+        this.dom_img.classList.add('movie_img');
+        node.appendChild(this.dom_element);
 
     }
 }
 class ListMovie {
-    constructor(search_str) {
+    constructor(search_str, node) {
         this.movies = [];
         this.page = 1;
         this.search_str = search_str;
         this.loading_next();
-        this.element_page = 4;
+        this.element_page = 1;
+        this.element_loading = 0;
+        this.element_loadinglodingcomplit = 0;
+        this.node = document.querySelector('.slider__wrapper');
+        this.cleardom();
+    }
+
+    cleardom() {
+        this.node.innerHTML = '';
+        document.querySelector('.loding').style.display = 'block';
     }
     async loading_next() {
+
         let moviesmass = await getMovie(this.search_str, this.page);
         this.page++;
-        moviesmass.Search.forEach(async element => {
-            let reiting = await getRerating(element.imdbID);
-            this.movies.push(new Movie(element.Title, element.Poster, element.Year, reiting.imdbRating));
-        });
+        for (let item of moviesmass.Search) {
+            {
+                this.element_loading++
+                    let reiting = await getRerating(item.imdbID);
+                this.movies.push(new Movie(item.Title, item.Poster, item.Year, reiting.imdbRating));
+                this.element_loadinglodingcomplit++;
+            }
+        }
+
+        setTimeout(() => {
+            do {
+                if (this.element_loading === this.element_loadinglodingcomplit) {
+                    this.movies.forEach(element => {
+                        if (!element.domadd) {
+                            let moviesa = document.createElement('div');
+                            moviesa.classList.add('slider__item');
+                            document.querySelector('.slider__wrapper').appendChild(moviesa);
+                            element.dom_add(moviesa);
+                            element.domadd = true;
+                        };
+                    });
+                }
+            } while (this.element_loading != this.element_loadinglodingcomplit)
+            slider = new multiItemSlider('.slider');
+            document.querySelector('.loding').style.display = 'none';
+        }, 0);
 
     }
-    add_dom() {
-        {}
-        while (this.movies === []);
-    }
+
 
 
 }
 
 
-let d = new ListMovie('men in Black');
+let listMovie = new ListMovie('avengers', 'slider__wrapper');
+document.querySelector('.button__search').addEventListener('click', () => {
+    listMovie = new ListMovie(document.querySelector('#inpur__search').value, 'slider_wrapper');
+});
